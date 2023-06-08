ef721478efdab3eb01e38c571046e58f840df714
dalanndann <danncodes23@gmail.com>
2023-06-09 00:01:14 +0800

    //I don't understand yet what this is for.

---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+/*I already had "Example" earlier, published it on GitHub,
+made changes, and couldn't Commit because of a git config error I couldn't solve.
+So, I deleted "Example" and decided to try Hello World myself.
+*/
+class HelloWorld {
+    public static void main(String args[]) {
+        System.out.println("Hello World");
+        // I don't know what "x" means. Maybe I'll set it to hidden later.
+    }
+}
