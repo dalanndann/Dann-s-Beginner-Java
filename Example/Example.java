20a58032c022fd358be4f91eb0154d8a3c474a60
dalanndann_ <135991391+dalanndann@users.noreply.github.com>
2023-06-09 16:59:55 +0800

    Organize into folders

---
+++ Example/Example.java
@@ -0,0 +1,18 @@
+package Example;
+
+/*
+ This is a simple Java program.
+ We will call this file Example.java.
+ I am tweaking some comments from my reference. :D
+*/
+class Example {
+    // A Java program begins with a call to main().
+    // I don't know what "a call to main()" means yet.
+    // This is actually my second time doing Example.java.
+    public static void main(String[] args) {
+        System.out.println("Java drives the Web.");
+        // Also: Java drives me crazy.
+    }
+}
+
+// I don't understand why it doesn't compile the first time around
