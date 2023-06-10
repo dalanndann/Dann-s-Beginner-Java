2266bbcebadbbad43704c0ead45225de7e8d0812
dalanndann_ <135991391+dalanndann@users.noreply.github.com>
2023-06-10 11:50:37 +0800

    Create IfDemo

---
+++ IfDemo.java
@@ -0,0 +1,39 @@
+/*
+    Demonstrate the if.
+    
+    Call this file IfDemo.java.
+*/
+class IfDemo {
+    public static void main(String[] args) {
+        int a, b, c;
+
+        a = 2;
+        b = 3;
+
+        if (a < b)
+            System.out.println("a is less than b");
+
+        // this won't display anything
+        if (a == b)
+            System.out.println("you won't see this");
+        // because a is not equal to b
+
+        System.out.println();
+
+        c = a - b; // c contains -1
+
+        System.out.println("c contains -1");
+        if (c >= 0)
+            System.out.println("c is non-negative");
+        if (c < 0)
+            System.out.println("c is negative");
+
+        c = b - a; // c now contains 1
+
+        System.out.println("c contains 1");
+        if (c >= 0)
+            System.out.println("c is non-negative");
+        if (c < 0)
+            System.out.println("c is negative");
+    }
+}
