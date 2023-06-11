b69d1ac094f997c57ec3ba0616dd3c9c393d1ad3
dalanndann_ <135991391+dalanndann@users.noreply.github.com>
2023-06-11 15:47:28 +0800

    Create notes on semicolons and positioning

---
+++ GalToLitTable.java
@@ -0,0 +1,44 @@
+/*
+Try This 1-2 
+
+This program displays a conversion table of gallons to liters.
+
+Call this program "GalToLitTable.java".
+*/
+
+class GalToLitTable {
+    public static void main(String[] args) {
+        double gallons, liters;
+        int counter;
+
+        counter = 0;
+        for (gallons = 1; gallons <= 100; gallons++) {
+            liters = gallons * 3.7854; // convert to liters
+            System.out.println(gallons + " gallons is " + liters + " liters.");
+
+            counter++;
+            // every 10th line, print a blank line
+            if (counter == 10) {
+                System.out.println();
+                counter = 0; // reset the line counter
+            }
+        }
+    }
+}
+
+/*
+ * Dann's notes
+ * Prior to inputting the code, I did not understand the purpose and function of
+ * counter. Here's my understanding of its purpose:
+ * 
+ * initially, counter will be zero
+ * then, we input the for loop where the first line will be the value of 1
+ * gallon in liters
+ * for every line printed, the counter will increase by 1
+ * this will continue until the 10th line.
+ * 
+ * 10th line - a blank line will be printed, the counter will be reset to 0
+ * 
+ * the for loop will be repeated until the hundreth gallon
+ * 
+ */
---
+++ Notes - Semicolons and positioning.md
@@ -0,0 +1,20 @@
+Semicolon - separator
+
+- often used to terminate a statement
+- indicates the end of one logical entity
+
+Block - set of logically connected statements that are surrounded by opening and closing braces
+
+- not terminated with a semicolon (end of a block is indicated by the closing brace)
+
+End of the line is not recognized as a terminator.
+
+x = y;
+y = y + 1;
+System.out.println(x + " " + y);
+
+is same as:
+
+x = y; y = y + 1; System.out.println(x + " " + y);
+
+Indentation - indent one level after each opening brace, move back out one level after each closing brace
