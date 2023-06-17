a648a9fe0486fc9b444d0a9a10d31a3d44329155
dalanndann_ <135991391+dalanndann@users.noreply.github.com>
2023-06-17 17:04:11 +0800

    Add notes on 7.2 and 1100 values

--- Sound.java
+++ Sound.java
@@ -3,6 +3,9 @@ class Sound {
         double distance;
 
         distance = 7.2 * 1100;
+        // time interval from seeing lightning to hearing its sound is assumed to be
+        // 7.2s
+        // speed of sound through air is 1100 feet per second
 
         System.out.println("The lightning is " + distance + " feet away.");
     }
