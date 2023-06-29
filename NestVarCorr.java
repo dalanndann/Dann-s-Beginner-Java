59910d016ecd33b26ccd5a7ec365020a6ff2e160
dalanndann_ <135991391+dalanndann@users.noreply.github.com>
2023-06-29 23:14:38 +0800

    Add notes Initial mistakes Second test + result + mistakes

--- NestVarCorr.java
+++ NestVarCorr.java
@@ -8,17 +8,32 @@ class NestVarCorr {
             System.out.println("This is count: " + count);
 
             int count2 = 0;
-            for (count = 0; count < 2; count++)
-                count2 = count + 1;
-            System.out.println("Count 2 is: " + count2);
+            for (count2 = 0; count2 < 2; count2++)
+                System.out.println("(hidden)");
         }
     }
 }
 
 /*
  * Dann's notes
+ * 1)
  * for loop is neverending
  * for (count = 0; count < 2; count++)
  * count2 = count + 1;
  * System.out.println("Count 2 is: " + count2);
+ * 
+ * My mistakes:
+ * forgot the for loop Ln7
+ * 
+ * 2) attempt to hide count if less than 2
+ * for (count = 0; count < 10; count = count + 1) {
+ * System.out.println("This is count: " + count);
+ * 
+ * int count2 = 0;
+ * for (count2 = 0; count2 < 2; count2++)
+ * System.out.println("(hidden)");
+ * 
+ * My mistakes:
+ * for loop Ln11 should remain count1 (maybe)
+ * 
  */
