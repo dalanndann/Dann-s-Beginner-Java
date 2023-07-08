6f0064e113ca32170479ff01fc235b9271745c5a
dalanndann_ <135991391+dalanndann@users.noreply.github.com>
2023-07-08 19:24:21 +0800

    Add if statement with & operator

--- SCops.java
+++ SCops.java
@@ -1,7 +1,7 @@
 // Demonstrate the short-circuit operators
 class SCops {
     public static void main(String[] args) {
-        int n, d, q;
+        int n, d;
 
         n = 10;
         d = 2;
@@ -11,9 +11,19 @@ class SCops {
         d = 0; // now set d to 0
         if (d != 0 && (n % d) == 0) // the SCop prevents a division by zero
             System.out.println(d + " is a factor of " + n);
+
+        // regular operator, not short-circuit
+        if (d != 0 & (n % d) == 0)
+            System.out.println(d + " is a factor of " + n);
     }
 }
 
 // DANN'S NOTES
 // When d was set to 0, d was no longer a factor of n
 // Therefore, cannot print 0 is a factor of 10
+
+/*
+ * w/o SCop
+ * Exception in thread "main" java.lang.ArithmeticException: / by zero at
+ * SCops.main(SCops.java:16)
+ */
